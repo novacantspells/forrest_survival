e114833b6a58f56c430b78683f9f6fae5d6a25ca
nova <novacantspell@gmail.com>
2022-09-14 13:23:03 +0100

    001: slow type function

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys,time,random
+
+typing_speed = 50 #wpm
+def slow_type(t):
+    for l in t:
+        sys.stdout.write(l)
+        sys.stdout.flush()
+        time.sleep(random.random()*10.0/typing_speed)
+    print ('')
+
